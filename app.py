1c71b7c050d662920804475840dbb14f8bbe38a9
ibrahimzademiribrahim6-bit <ibrahimzademiribrahim6@gmail.com>
2026-08-23 13:02:08 +0400

    init_db modul seviyyesinde cagirildi, gitignore duzeldildi

--- app.py
+++ app.py
@@ -3393,6 +3393,7 @@ def init_db():
             old_suggestion.name = "Təkliflər"
         db.session.commit()
 
+init_db()
+
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True, port=5000)
